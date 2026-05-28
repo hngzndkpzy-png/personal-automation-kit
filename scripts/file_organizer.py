0dd34b818f2822c87972fbd8f9811318349ed08b
hngzndkpzy-png <hngzndkpzy@privaterelay.appleid.com>
2026-05-28 04:14:33 +0200

    Add file organizer script

---
+++ scripts/file_organizer.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+def organize_files(folder='Downloads'):
+    print(f'Organizing files in {folder} - ready for customization')
+
+if __name__ == '__main__':
+    organize_files()
